fb8ee183931dca580ac54784e24f39de5a57e32c
Alexander Kasper <foo@hallo.de>
2018-02-04 00:45:16 +0100

    notes

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+"""
+ some notes on tests
+
+https://www.pythonsheets.com/notes/python-tests.html
+https://docs.python.org/3/library/unittest.html
+https://docs.python.org/3/library/unittest.mock.html#unittest.mock.patch
+"""
