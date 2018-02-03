60259b6904ded11df9e2995cca4e1812175514d3
Alexander Kasper <foo@hallo.de>
2018-02-03 14:13:01 +0100

    refactoring connection to use BorgPattern

--- src/amqppublisher/connection.py
+++ src/amqppublisher/connection.py
@@ -6,47 +6,62 @@ import sys
 
 log = logging.getLogger(__name__)
 
-def setupConnection(args):
-    """
-    @result: returns a pika channel
-    """
+class AMQPConnectionBorg:
+    _shared_state = {}
+    def __init__(self,args):
+        self.__dict__ = self.__class__._shared_state
 
-    parameters=None
-    ch = None
+        if not 'ch' in self.__dict__:
+            self.ch = None
+        if not 'con' in self.__dict__:
+            self.con = None
 
-    ssl_options = dict(
-        certfile = args.certfile,
-        keyfile = args.keyfile,
-        ca_certs = args.cacert,
-        cert_reqs = CERT_REQUIRED )
+        if not 'config' in self.__dict__:
+            self.config = args
+        if not 'ssl_options' in self.__dict__:
+            self.ssl_options = dict(
+                certfile = self.config.certfile,
+                keyfile = self.config.keyfile,
+                ca_certs = self.config.cacert,
+                cert_reqs = CERT_REQUIRED )
+        if not 'parameters' in self.__dict__:
+            if self.config.tls:
+                self.parameters=ConnectionParameters(
+                    host = self.config.host,
+                    port = self.config.port,
+                    virtual_host = self.config.vhost,
+                    connection_attempts = self.config.retry,
+                    retry_delay = self.config.retrydelay,
+                    socket_timeout = self.config.sockettimeout,
+                    credentials = ExternalCredentials(),
+                    ssl = True,
+                    ssl_options = self.ssl_options)
+            else:
+                self.parameters=ConnectionParameters(
+                    host = self.config.host,
+                    port = self.config.port,
+                    virtual_host = self.config.vhost,
+                    connection_attempts = self.config.retry,
+                    retry_delay = self.config.retrydelay,
+                    socket_timeout = self.config.sockettimeout,
+                    credentials = PlainCredentials(username=self.config.username,
+                                                   password=self.config.password),
+                    ssl = True)
+        print(id(self.__dict__))
 
-    if args.tls:
-        parameters=ConnectionParameters(
-            host = args.host,
-            port = args.port,
-            virtual_host = args.vhost,
-            connection_attempts = args.retry,
-            retry_delay = args.retrydelay,
-            socket_timeout = args.sockettimeout,
-            credentials = ExternalCredentials(),
-            ssl = True,
-            ssl_options = ssl_options)
-    else:
-        parameters=ConnectionParameters(
-            host = args.host,
-            port = args.port,
-            virtual_host = args.vhost,
-            connection_attempts = args.retry,
-            retry_delay = args.retrydelay,
-            socket_timeout = args.sockettimeout,
-            credentials = PlainCredentials(username=args.username,
-                                           password=args.password),
-            ssl = True)
-    try:
-        con=BlockingConnection(parameters)
-        ch=con.channel()
-    except Exception as e:
-        log.critical("could not connect to server with paramters: %s\n  last Exception: %s\n  Additional arguments:%s" %(parameters,e,args))
-        sys.exit(1)
+    def getConnection(self):
+        """
+        @result: returns a pika channel
+        """
+        
+        if all([self.ch,self.con]):
+            return (self.ch,self.con)
 
-    return ch
+        try:
+            self.con = BlockingConnection(self.parameters)
+            self.ch = con.channel()
+        except Exception as e:
+            log.critical("could not connect to server with paramters: %s\n  last Exception: %s\n  Additional arguments:%s" %(parameters,e,args))
+            sys.exit(1)
+
+        return (ch,con)
